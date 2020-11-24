e337d983cdff8cd842167b112f9c2e4252fbdb77
amit <amitwave@gmail.com>
2020-11-24 18:26:05 +0000

    stream merge

--- src/main/java/com/wave/kafka/streams/simple/SimpleDateSink.java
+++ src/main/java/com/wave/kafka/streams/simple/SimpleDateSink.java
@@ -8,20 +8,17 @@ import org.springframework.stereotype.Component;
 
 import static com.wave.kafka.streams.WaveProcessorBinding.*;
 
-
-//@EnableBinding(KStreamProcessor.class)
 @EnableBinding(WaveProcessorBinding.class)
-@Component
 public class SimpleDateSink {
     @StreamListener(INPUTSTREAMSTRING)
     public void handle(String s) {
-        System.out.println("In the UppercaseSink SINK  88 " + s);
+        System.out.println("In the SimpleDateSink SINK  88 " + s);
 
     }
 
     @StreamListener(INPUTUSER)
     public void handle1(User s) {
-        System.out.println("In the UppercaseSink user SINK  99 " + s);
+        System.out.println("In the SimpleDateSink user SINK  99 " + s);
 
     }
 
